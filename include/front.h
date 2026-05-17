ca491fc1a54d901d5aa1d0038652280b6cf1ed88
apopova7-netizen <a.popova7@g.nsu.ru>
2026-05-17 20:31:38 +0700

    Create front.h

---
+++ include/front.h
@@ -0,0 +1,13 @@
+#ifndef GRAPH_MASTER_FRONT1_H
+#define GRAPH_MASTER_FRONT1_H
+#include "common1.h"
+
+void Dijkstra(Graph *g, const int *path, int pathLen, int totalWeight, double timeMS);
+char* AskFilename();
+int Menu();
+void PrintGraphInfo(int numVertices,int numEdges);
+void PrintComparison(const Comparison* cmp);
+void PrintBFS(Graph *g, int *order, int orderLen, double timeMS);
+void PrintMST(const MSTResult *res, Graph *g);
+
+#endif //GRAPH_MASTER_FRONT1_H
